e07c80a7ae6be88d95e1179a0fff582f811f7de8
98prabowo <58059518+98prabowo@users.noreply.github.com>
2025-02-18 13:12:27 +0700

    feat(keychain): create constants for security key

---
+++ appika-keychain/src/constants.rs
@@ -0,0 +1,15 @@
+// Class
+pub const SECURITY_KEY_CLASS: &str = "kSecClass";
+pub const SECURITY_KEY_CLASS_GENERIC_PASSWORD: &str = "kSecClassGenericPassword";
+
+// Attribute key
+pub const SECURITY_KEY_ATTRIBUTE_SERVICE: &str = "kSecAttrService";
+pub const SECURITY_KEY_ATTRIBUTE_ACCOUNT: &str = "kSecAttrAccount";
+
+// Payload key
+pub const SECURITY_KEY_VALUE_DATA: &str = "kSecValueData";
+pub const SECURITY_KEY_RETURN_DATA: &str = "kSecReturnData";
+
+// Keychain query match limit
+pub const SECURITY_KEY_MATCH_LIMIT: &str = "kSecMatchLimit";
+pub const SECURITY_KEY_MATCH_LIMIT_ONE: &str = "kSecMatchLimitOne";
