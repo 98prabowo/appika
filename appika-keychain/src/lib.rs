16d408b0f2adf12df9148f34b4b961f70e235c07
98prabowo <58059518+98prabowo@users.noreply.github.com>
2025-02-18 13:13:54 +0700

    feat(keychain): export keychain package

--- appika-keychain/src/lib.rs
+++ appika-keychain/src/lib.rs
@@ -1,3 +1,12 @@
+mod constants;
+mod error;
+mod ffi;
+mod os_status;
+mod service;
+
+pub use error::{Result, Error};
+pub use service::KeychainService;
+
 pub trait Keychain {
     fn store_password(&self, service: &str, account: &str, password: &str) -> error::Result<()>;
     fn get_password(&self, service: &str, account: &str) -> error::Result<String>;
