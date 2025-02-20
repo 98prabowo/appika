72e6b49cd5ffc4b6f72f05f66d7a01e89316ac26
98prabowo <58059518+98prabowo@users.noreply.github.com>
2025-02-20 15:52:55 +0700

    fix(keychain): use security key from `security-framework` instead of creating manually

--- appika-keychain/src/constants.rs
+++ appika-keychain/src/constants.rs
@@ -1,15 +1,30 @@
-// Class
-pub const SECURITY_KEY_CLASS: &str = "kSecClass";
-pub const SECURITY_KEY_CLASS_GENERIC_PASSWORD: &str = "kSecClassGenericPassword";
+use core_foundation::{base::TCFType, string::CFString};
+use security_framework_sys::item::*;
 
-// Attribute key
-pub const SECURITY_KEY_ATTRIBUTE_SERVICE: &str = "kSecAttrService";
-pub const SECURITY_KEY_ATTRIBUTE_ACCOUNT: &str = "kSecAttrAccount";
+pub fn security_key_class() -> CFString { 
+    unsafe { CFString::wrap_under_get_rule(kSecClass) }
+}
 
-// Payload key
-pub const SECURITY_KEY_VALUE_DATA: &str = "kSecValueData";
-pub const SECURITY_KEY_RETURN_DATA: &str = "kSecReturnData";
+pub fn security_key_class_generic_password() -> CFString { 
+    unsafe { CFString::wrap_under_get_rule(kSecClassGenericPassword) }
+}
 
-// Keychain query match limit
-pub const SECURITY_KEY_MATCH_LIMIT: &str = "kSecMatchLimit";
-pub const SECURITY_KEY_MATCH_LIMIT_ONE: &str = "kSecMatchLimitOne";
+pub fn security_key_attribute_service() -> CFString { 
+    unsafe { CFString::wrap_under_get_rule(kSecAttrService) }
+}
+
+pub fn security_key_attribute_account() -> CFString { 
+    unsafe { CFString::wrap_under_get_rule(kSecAttrAccount) }
+}
+
+pub fn security_key_value_data() -> CFString { 
+    unsafe { CFString::wrap_under_get_rule(kSecValueData) }
+}
+
+pub fn security_key_return_data() -> CFString {
+    unsafe { CFString::wrap_under_get_rule(kSecReturnData) }
+}
+
+pub fn security_key_match_limit() -> CFString {
+    unsafe { CFString::wrap_under_get_rule(kSecMatchLimit) }
+}
