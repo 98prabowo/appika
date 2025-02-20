0d7484b1b78c84fe6c0e35d1c03f45fd7079c5e3
98prabowo <58059518+98prabowo@users.noreply.github.com>
2025-02-20 11:59:46 +0700

    feat(keychain): add unit test to `OSStatus`

--- appika-keychain/src/os_status.rs
+++ appika-keychain/src/os_status.rs
@@ -21,3 +21,89 @@ pub fn error_description(status: OSStatus) -> String {
         get_description(status)
     )
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_unspecified_error() {
+        let status: OSStatus = 1;
+        let desc: &str = "Unspecified error";
+        let error: String = format!("Operation failed with OS status code 1: {}", desc);
+        assert_eq!(get_description(status), desc);
+        assert_eq!(error_description(status), error);
+    }
+
+    #[test]
+    fn test_memory_error() {
+        let status: OSStatus = 2;
+        let desc: &str = "Memory error";
+        let error: String = format!("Operation failed with OS status code 2: {}", desc);
+        assert_eq!(get_description(status), desc);
+        assert_eq!(error_description(status), error);
+    }
+
+    #[test]
+    fn test_parameter_error() {
+        let status: OSStatus = 3;
+        let desc: &str = "Param error";
+        let error: String = format!("Operation failed with OS status code 3: {}", desc);
+        assert_eq!(get_description(status), desc);
+        assert_eq!(error_description(status), error);
+    }
+
+    #[test]
+    fn test_invalid_handle_error() {
+        let status: OSStatus = 4;
+        let desc: &str = "Invalid handle";
+        let error: String = format!("Operation failed with OS status code 4: {}", desc);
+        assert_eq!(get_description(status), desc);
+        assert_eq!(error_description(status), error);
+    }
+
+    #[test]
+    fn test_not_found_error() {
+        let status: OSStatus = 5;
+        let desc: &str = "Not found";
+        let error: String = format!("Operation failed with OS status code 5: {}", desc);
+        assert_eq!(get_description(status), desc);
+        assert_eq!(error_description(status), error);
+    }
+
+    #[test]
+    fn test_access_denied_error() {
+        let status: OSStatus = 6;
+        let desc: &str = "Access denied";
+        let error: String = format!("Operation failed with OS status code 6: {}", desc);
+        assert_eq!(get_description(status), desc);
+        assert_eq!(error_description(status), error);
+    }
+
+    #[test]
+    fn test_invalid_data_error() {
+        let status: OSStatus = 7;
+        let desc: &str = "Data invalid";
+        let error: String = format!("Operation failed with OS status code 7: {}", desc);
+        assert_eq!(get_description(status), desc);
+        assert_eq!(error_description(status), error);
+    }
+
+    #[test]
+    fn test_authentication_error() {
+        let status: OSStatus = 8;
+        let desc: &str = "Authentication failed";
+        let error: String = format!("Operation failed with OS status code 8: {}", desc);
+        assert_eq!(get_description(status), desc);
+        assert_eq!(error_description(status), error);
+    }
+
+    #[test]
+    fn test_unknown_error() {
+        let status: OSStatus = 100;
+        let desc: &str = "Unknown error";
+        let error: String = format!("Operation failed with OS status code 100: {}", desc);
+        assert_eq!(get_description(status), desc);
+        assert_eq!(error_description(status), error);
+    }
+}
